a663c2d1f1ad2a319bc8806153c957f7eb9a776e
Bruno Gianini <blgianini@gmail.com>
2025-03-25 21:29:10 -0300

    feat: login page e regra de redirect

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/components/login-form";
+
+export default function Login(){
+
+    return(
+        <div className="h-screen flex justify-center items-center px-5">
+            <LoginForm />
+        </div>
+    )
+}
---
+++ src/components/login-form.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { signIn } from "next-auth/react"
+
+export default function LoginForm(){
+    async function login(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+
+        const data = {
+            email: formData.get("email"),
+            senha: formData.get("senha")
+        }
+
+        signIn("credentials", {
+            ...data,
+            callbackUrl: '/'
+        })
+    }
+
+    return(
+        <form onSubmit={login}>
+            <Card className="p-4 flex gap-8 w-96 h-96 justify-center">
+                <CardHeader>
+                    <CardTitle className="text-2xl font-bold tracking-tighter">Entre com a sua conta</CardTitle>
+                    <CardDescription>Ultilize seu email e senha</CardDescription>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-5">
+                    <div>
+                        <Label htmlFor="email">Email</Label>
+                        <Input name="email" id="email" placeholder="exemplo@email.com" type="email" />
+                    </div>
+                    <div>
+                        <Label htmlFor="senha">Senha</Label>
+                        <Input name="senha" id="senha" placeholder="senha" type="password" />
+                    </div>
+
+                    <Button className="mt-6 w-full cursor-pointer">Entrar</Button>
+                </CardContent>
+            </Card>
+        </form>
+    )
+}
